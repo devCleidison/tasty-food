7b99eae53e4052444154efec11c2e320d98eb319
Cleidison Silva <cleidison.dev@gmail.com>
2021-11-11 06:51:07 -0300

    Header concluído

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId)
+  const nav = document.getElementById(navId)
+
+  if(toggle && nav){
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show-menu')
+    })
+  }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    let sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else{
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+
+
+function scrollHeader(){
+  const nav = document.getElementById('header')
+
+  if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+
+
+function scrollTop(){
+  const scrollTop = document.getElementById('scroll-top')
+
+  if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
+}
+
+window.addEventListener('scroll', scrollTop)
