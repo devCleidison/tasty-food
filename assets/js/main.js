08a5c3f36d5f689351cc02eec61f59853711dc50
Cleidison Silva <cleidison.dev@gmail.com>
2021-11-12 05:43:45 -0300

    Add efeitos de transição

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,76 @@
 const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId)
-  const nav = document.getElementById(navId)
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
 
-  if(toggle && nav){
-    toggle.addEventListener('click', () => {
-      nav.classList.toggle('show-menu')
-    })
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
   }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-showMenu('nav-toggle', 'nav-menu')
+const sections = document.querySelectorAll("section[id]");
 
-const navLink = document.querySelectorAll('.nav__link')
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-function linkAction(){
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    let sectionId = current.getAttribute("id");
 
-
-
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive(){
-  const scrollY = window.pageYOffset
-
-  sections.forEach(current => {
-    const sectionHeight = current.offsetHeight
-    const sectionTop = current.offsetTop - 50
-    let sectionId = current.getAttribute('id')
-
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-    }else{
-      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
     }
-  })
+  });
 }
 
+function scrollHeader() {
+  const nav = document.getElementById("header");
 
-function scrollHeader(){
-  const nav = document.getElementById('header')
-
-  if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
 
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
 
-
-function scrollTop(){
-  const scrollTop = document.getElementById('scroll-top')
-
-  if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
+  if (this.scrollY >= 560) scrollTop.classList.add("scroll-top");
+  else scrollTop.classList.remove("scroll-top");
 }
 
-window.addEventListener('scroll', scrollTop)
+window.addEventListener("scroll", scrollTop);
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(`.home__data, .home__img, 
+          .about__data, .about__img, 
+          .services__content, .menu__content, 
+          .app__data, .app__img, 
+          .contact__data, .contact__button, 
+          .footer__content`, {
+  interval: 200,
+});
